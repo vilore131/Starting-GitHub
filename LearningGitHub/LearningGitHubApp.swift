6f0490c1e15cbe6e9310d51bda23af81beb605cd
Edwin Viloria <vilore131@hotmail.com>
2022-04-29 10:11:29 -0400

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Edwin Viloria on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
